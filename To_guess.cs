e21000a33a9ffb1d8508b55df269c21854ceecab
Arckanna <valerie.lecoeur75@gmail.com>
2024-01-14 13:52:22 +0100

    creation class to_guess

---
+++ To_guess.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpfTest
+{
+    internal class To_guess
+    {
+        public static string[] words = { "hello", "world", "test", "guess" };
+        private string wordToGuess;
+        private char letterProposed;
+        private string wordAdvance;
+        private int nbTry;
+
+        public string InitializeWordToGuess()
+        {
+            this.wordAdvance = "";
+            int len = this.wordLenght;
+            for (int i = 0; i < len; i++)
+            {
+                this.wordAdvance += "_";
+            }
+            return this.wordAdvance;
+        }   
+
+        public bool IsWordFound()
+        {
+            if (this.wordAdvance == this.wordToGuess)
+                return true;
+            return false;
+        }
+
+        public To_guess(string value)
+        {
+            this.wordToGuess = value;
+            InitializeWordToGuess();
+            this.nbTry = 0;
+        }
+
+        public To_guess()
+        {           
+            this.wordToGuess = wordToGuess[1];
+            InitializeWordToGuess();
+            this.nbTry = 0;
+        }
+
+        public int NbTry {
+            get { return this.nbTry; }
+            set { this.nbTry = value; }
+        }
+
+        public string WordToGuess
+        {
+            get { return this.wordToGuess; }
+            set { this.wordToGuess = value; }
+        }
+
+        public string WordAdvance
+        {
+            get { return this.wordAdvance; }
+            set { this.wordAdvance = value; }
+        }
+
+        public int wordLenght
+        {
+            get { return WordToGuess.Length; }
+        }
+
+        public char LetterProposed
+        {
+            get { return this.letterProposed; }
+            set { this.letterProposed = value; }
+        }
+
+    }
+}
